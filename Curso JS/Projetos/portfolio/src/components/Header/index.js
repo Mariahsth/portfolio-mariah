1aacf63fa1511aad2d25106d4e5c31523a1b5e3c
Mariah Hoffmann <mariah.hoffmann@gmail.com>
2025-08-05 13:19:44 -0300

    corrigindo menu hamburguer

--- Curso JS/Projetos/portfolio/src/components/Header/index.js
+++ Curso JS/Projetos/portfolio/src/components/Header/index.js
@@ -18,6 +18,12 @@ export default function Header(){
         document.addEventListener("mousedown", handleClickOutside);
         return () => document.removeEventListener("mousedown", handleClickOutside);
       }, [menuOpen]);
+    
+    const handleNavClick = () => {
+    if (window.innerWidth <= 455) {
+        setMenuOpen(false);
+    }
+    };
 
     return(
         <header>
@@ -27,10 +33,10 @@ export default function Header(){
             <nav  >
                 <p ><a className="logo" href="#home">&lt;M/H&gt;</a></p>
                 <ul $isOpen={menuOpen} ref={navRef} className={`header_menu ${menuOpen ? 'open' : 'closed'}`}>
-                    <li><a href="#home">Início</a></li>
-                    <li><a href="#about">Sobre mim</a></li>
-                    <li><a href="#tecnologies">Tecnologias</a></li>
-                    <li><a href="#projects">Projetos</a></li>
+                    <li><a href="#home" onClick={handleNavClick}>Início</a></li>
+                    <li><a href="#about" onClick={handleNavClick}>Sobre mim</a></li>
+                    <li><a href="#tecnologies" onClick={handleNavClick}>Tecnologias</a></li>
+                    <li><a href="#projects" onClick={handleNavClick}>Projetos</a></li>
                 </ul>
             </nav>
         </header>
